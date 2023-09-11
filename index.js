4d4784bdb1b44af91520a7e31bed508934db4afb
harshal655 <harshal@starkenn.com>
2023-09-11 15:00:17 +0530

    addedon11thsept

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const { featuresetRouter } = require("./routes/admin/featuresetRoute");
 const {
   vehiclefeaturesetRouter,
 } = require("./routes/customer/vehicleFeaturesetRoute");
+const { alertRouter } = require("./routes/customer/alerttriggersRoute");
 
 require("dotenv").config();
 const PORT = process.env.PORT;
@@ -54,6 +55,7 @@ app.use("/api/drivers", driversRouter);
 app.use("/api/profile", profileRouter);
 app.use("/api/rfid", rfidRouter);
 app.use("/api/vehicle-featureset", vehiclefeaturesetRouter);
+app.use("/api/alert-triggers", alertRouter);
 
 app.listen(PORT, () => {
   logger.info(`App is running on port ${PORT}`);
