df8d45569296fb1a3dcf7999df89b11002c5a123
harshal655 <harshal@starkenn.com>
2023-10-13 17:55:39 +0530

    addedon13thevening

--- controllers/customer/reportsController.js
+++ controllers/customer/reportsController.js
@@ -4,13 +4,8 @@ const express = require("express");
 const moment = require("moment-timezone");
 const { v4: uuidv4 } = require("uuid");
 const bodyParser = require("body-parser");
-<<<<<<< HEAD
-// const { createAndScheduleReport } = require('../path-to-createAndScheduleReport');
-=======
-const schedule = require('node-schedule');
+const schedule = require("node-schedule");
 //const { createAndScheduleReport } = require('../path-to-createAndScheduleReport');
-
->>>>>>> main
 
 const { sendEmail } = require("../../middleware/mailer");
 const { sendWhatsappMessage } = require("../../middleware/whatsapp");
@@ -341,7 +336,7 @@ exports.createAllreport = async (req, res) => {
       success: true,
       message: "Report data successfully created",
       report_uuid: newUuid,
-      events: selectedEventsJson
+      events: selectedEventsJson,
     });
   } catch (err) {
     logger.error(`Error in creating report: ${err.message}`);
@@ -373,24 +368,21 @@ exports.getReports = async (req, res) => {
     }
 
     const report = reportResult[0];
-    const { from_date: fromDate, to_date: toDate, selected_events: reportSelectedEvents } = report;
+    const {
+      from_date: fromDate,
+      to_date: toDate,
+      selected_events: reportSelectedEvents,
+    } = report;
 
     const vehiclesData = JSON.parse(report.vehicles);
     const vehicle_uuids = Object.keys(vehiclesData).map(
       (vehicleKey) => vehiclesData[vehicleKey].vehicle_uuid
     );
 
-<<<<<<< HEAD
-    events = events ? events.split(",") : [];
-    const selectedEvents =
-      events.length > 0 ? events : ["ACC", "ACD", "DMS", "LMP"];
-=======
     const selectedEvents = Array.isArray(reportSelectedEvents)
-    ? reportSelectedEvents
-    : JSON.parse(reportSelectedEvents);
+      ? reportSelectedEvents
+      : JSON.parse(reportSelectedEvents);
 
-
->>>>>>> main
     const vehicleResults = await Promise.all(
       vehicle_uuids.map(async (vehicle_uuid) => {
         const vehicleData = vehiclesData[vehicle_uuid];
@@ -410,7 +402,7 @@ exports.getReports = async (req, res) => {
           toDate,
           ...selectedEvents,
         ]);
-//console.log(tripdataResult);
+        //console.log(tripdataResult);
         return {
           vehicle_uuid,
           vehicle_name: vehicleData.vehicle_name,
@@ -421,17 +413,12 @@ exports.getReports = async (req, res) => {
     );
 
     res.status(200).send({
-<<<<<<< HEAD
       message:
         "Successfully retrieved report and tripdata for multiple vehicles",
-      report,
-=======
-      message: "Successfully retrieved report and tripdata for multiple vehicles",
       report: {
         ...report,
         selected_events: selectedEvents,
       },
->>>>>>> main
       vehicleResults,
     });
   } catch (err) {
@@ -445,102 +432,23 @@ exports.getReports = async (req, res) => {
   }
 };
 
-<<<<<<< HEAD
-exports.scheduleReports = async (req, res) => {
-  try {
-    // Parse request parameters
-    const { user_uuid } = req.params;
-    const {
-      title,
-      selected_events,
-      from_date,
-      to_date,
-      contact_uuid,
-      selected_vehicles,
-    } = req.body;
-=======
 // This Api Used For Schedule Reports
 // ...
->>>>>>> main
-
-
-<<<<<<< HEAD
-    // Schedule the report creation using node-schedule
-    const newUuid = uuidv4();
-    const createdAt = moment().tz("Asia/Kolkata").format("YYYY-MM-DD HH:mm:ss");
-
-    // Define the schedule for creating reports (modify as needed)
-    const scheduleTime = new Date(); // Replace with your desired schedule time
-    scheduleTime.setHours(1, 0, 0, 0); // Set the time to 01:00:00 AM
-
-    // Schedule the report creation
-    schedule.scheduleJob(scheduleTime, async () => {
-      const connection = await pool.getConnection();
-      try {
-        // Your existing report creation code goes here
-        // ...
-
-        // Respond with a success message or any relevant information
-        res.status(200).json({
-          success: true,
-          message: "Report scheduled for creation",
-          report_uuid: newUuid,
-        });
-      } catch (err) {
-        logger.error(`Error in scheduling report: ${err.message}`);
-        res.status(500).json({
-          success: false,
-          message: "An error occurred while scheduling the report",
-          error: err.message,
-        });
-      } finally {
-        connection.release(); // Release the database connection
-      }
-    });
-  } catch (err) {
-    logger.error(`Error in scheduling report: ${err.message}`);
-    res.status(500).json({
-      success: false,
-      message: "An error occurred while scheduling the report",
-      error: err.message,
-    });
-  }
-};
-
-// exports.scheduleReports = async (req, res) => {
-//   try {
-//     const { user_uuid } = req.params;
-//     const { title, selected_events, from_date, to_date, contact_uuid, selected_vehicles } = req.body;
-
-//     const result = await createAndScheduleReport(
-//       user_uuid,
-//       title,
-//       selected_events,
-//       from_date,
-//       to_date,
-//       contact_uuid,
-//       selected_vehicles
-//     );
-
-//     res.status(200).json(result);
-//   } catch (err) {
-//     res.status(500).json({
-//       success: false,
-//       message: 'An error occurred while scheduling the report',
-//       error: err.message,
-//     });
-//   }
-// }
-
-// module.exports = router;
-=======
 
 exports.scheduleReports = async (req, res) => {
-  const { from_date, to_date, user_uuid, title, selected_events, contact_uuid, selected_vehicles } = req.body;
+  const {
+    from_date,
+    to_date,
+    user_uuid,
+    title,
+    selected_events,
+    contact_uuid,
+    selected_vehicles,
+  } = req.body;
 
   try {
     // Trigger the scheduled job by name or identifier
-    const result = await schedule.scheduledJobs['yourJobName'].callback(
+    const result = await schedule.scheduledJobs["yourJobName"].callback(
       from_date,
       to_date,
       user_uuid,
@@ -569,7 +477,7 @@ exports.scheduleReports = async (req, res) => {
 //   try {
 //     //const { title, selected_events, from_date, to_date, contact_uuid, selected_vehicles} = req.body;
 //     //const { user_uuid } = req.params;
- 
+
 // // Validate input parameters
 // if (
 //   !Array.isArray(selected_events) ||
@@ -577,7 +485,7 @@ exports.scheduleReports = async (req, res) => {
 //   !to_date ||
 //   !title ||
 //   !contact_uuid ||
-//   !Array.isArray(selected_vehicles) || 
+//   !Array.isArray(selected_vehicles) ||
 //   selected_vehicles.length === 0
 //   ) {
 //   return res.status(400).json({ message: "Invalid request parameters" });
@@ -586,11 +494,11 @@ exports.scheduleReports = async (req, res) => {
 //    // Convert fromDate and toDate to Date objects and validate
 //    const fromDateObj = new Date(from_date);
 //    const toDateObj = new Date(to_date);
-  
+
 //    if (isNaN(fromDateObj) || isNaN(toDateObj)) {
 //    return res.status(400).json({ message: "Invalid date format" });
 //    }
-  
+
 //    // Ensure fromDate is before toDate
 //    if (fromDateObj >= toDateObj) {
 //    return res
@@ -608,30 +516,30 @@ exports.scheduleReports = async (req, res) => {
 //     v.vehicle_name,
 //     v.vehicle_registration,
 //     ts.trip_id,
-//     td.event AS event_type, 
+//     td.event AS event_type,
 //     COUNT(*) AS event_count,
 //     td.timestamp,
 //     JSON_UNQUOTE(JSON_EXTRACT(td.jsondata, '$.data.alert_type')) AS alert_type,
 //     JSON_UNQUOTE(JSON_EXTRACT(td.jsondata, '$.message')) AS message
 // FROM
-//     vehicles v 
+//     vehicles v
 // LEFT JOIN
 //     trip_summary ts ON v.vehicle_uuid = ts.vehicle_uuid
 // LEFT JOIN
 //     tripdata td ON ts.trip_id = td.trip_id
 // WHERE
-//     v.user_uuid = ? 
-//     AND v.vehicle_status = ? 
+//     v.user_uuid = ?
+//     AND v.vehicle_status = ?
 //     AND td.created_at >= ?
-//     AND td.created_at <= ? 
-//     AND ts.trip_status = ? 
+//     AND td.created_at <= ?
+//     AND ts.trip_status = ?
 //     AND td.event IN (${eventPlaceholders})
 //     AND v.vehicle_uuid IN (${vehiclePlaceholders})
 // GROUP BY
 //     v.vehicle_uuid,
 //     v.vehicle_name,
 //     v.vehicle_registration,
-//     td.event  
+//     td.event
 // ORDER BY
 //     v.vehicle_uuid ASC
 
@@ -646,7 +554,6 @@ exports.scheduleReports = async (req, res) => {
 //       ...selected_events,
 //       ...selected_vehicles,
 //     ]);
-
 
 //     // Group the data by vehicle_uuid
 //     const groupedData = vehicles.reduce((result, row) => {
@@ -663,7 +570,7 @@ exports.scheduleReports = async (req, res) => {
 //         result[key].events.push({
 //          // trip_id: row.trip_id,
 //           eventType: row.event_type,
-//           eventCount: row.event_count, 
+//           eventCount: row.event_count,
 //         });
 //       }
 //       return result;
@@ -702,4 +609,3 @@ exports.scheduleReports = async (req, res) => {
 //   // Trigger the scheduled job
 //   schedule.scheduledJobs.callback(from_date, to_date);
 // };
->>>>>>> main
